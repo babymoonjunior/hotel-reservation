a228d8d4ebc51c4d274e5af67de23ba17d52d808
BuildTools <unconfigured@null.spigotmc.org>
2023-09-05 08:37:28 +0700

    feat:login-Page:split client and server components

--- client/src/app/(Users)/(Register)/login/page.js
+++ client/src/app/(Users)/(Register)/login/page.js
@@ -1,5 +1,6 @@
 import '@/app/globals.css'
 import LoginFrom from '../../components/loginFrom.jsx';
+import Image from 'next/image.js';
 
 export const metadata = {
   title: 'Login Page',
@@ -8,8 +9,17 @@ export const metadata = {
 
 export default function LoginPage() {
   return (
-    <div>
+    <div className='flex text-gray-900 h-full w-full'>
       {/* ใส่ Component1 <RegisterForm /> ที่นี่ (Pond) */}
+      <div className=" flex-1 h-[924px] w-[708px] relative">
+        <Image
+          className=""
+          src="/loginphoto.svg"
+          objectFit="cover"
+          layout="fill"
+          alt="photo-login"
+        />
+      </div>
       <LoginFrom />
     </div>
   );
