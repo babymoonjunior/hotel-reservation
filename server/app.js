d2bb16a33d585722c1a0ac171cb0cf0aa2b7a396
Nu <nuuseforstudy@gmail.com>
2023-09-06 11:46:26 +0700

    feat: add api to see room

--- server/app.js
+++ server/app.js
@@ -4,31 +4,27 @@ import cors from "cors";
 // import postRouter from "./apps/posts.js";
 // import authRouter from "./apps/auth.js";
 import usersRouter from "./apps/users.js";
+import roomsRouter from "./apps/rooms.js";
 
 async function init() {
+  const app = express();
+  const port = 4000;
 
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use("/rooms", roomsRouter);
 
-    const app = express();
-    const port = 4000;
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
 
-    app.use(cors());
-    app.use(bodyParser.json());
-    // app.use("/posts", postRouter);
-    // app.use("/auth", authRouter);
-    app.use("/users", usersRouter);
+  app.get("*", (req, res) => {
+    res.status(404).send("Not Found");
+  });
 
-
-    app.get("/", (req, res) => {
-        res.send("Hello World!");
-    });
-
-    app.get("*", (req, res) => {
-        res.status(404).send("Not Found");
-    });
-
-    app.listen(port, () => {
-        console.log(`Server is running at ${port}`);
-    });
+  app.listen(port, () => {
+    console.log(`Server is running at ${port}`);
+  });
 }
 
 init();
