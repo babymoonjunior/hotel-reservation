aa9e65c98df158631dcb542d2959f56d243b4366
PreeyanuchYu <babymoonshop@gmail.com>
2023-09-07 00:13:08 +0700

    Feat: Edit SearchResults & Link to FullViewImage

--- client/src/app/(Users)/components/SearchResults.jsx
+++ client/src/app/(Users)/components/SearchResults.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import RoomDetails from "./RoomDetails";
 import PriceDetails from "./PriceDetails";
-import Link from 'next/link';
+import Link from "next/link";
 
 const roomDataArray = [
   {
@@ -60,49 +60,51 @@ const roomDataArray = [
 
 export default function SearchResultsPage() {
   return (
-    <div className="main-container bg-[#F7F7FB] w-full max-w-[1440px] h-auto p-[100px]">
-      {roomDataArray.map((roomData, index) => (
-        <div
-          key={index}
-          className="search-room-card bg-[#F7F7FB]  h-[400px] flex justify-between font-sans py-10 border-b border-[#E4E6ED]"
-        >
-          <div className="image-box w-full max-w-[453px] h-auto relative mr-8">
-            <Image
-              src={roomData.imageSrc}
-              alt={roomData.imageAlt}
-              width={453}
-              height={320}
-              className="object-cover h-full"
-            />
-            <Link href="/fullview">
-            <div className="transparent-bg w-10 h-10 bg-white flex items-center justify-center opacity-50 absolute bottom-0">
-              <div className="frame-link-image">
-                <Image
-                  src="/Frame.png"
-                  alt="See more image"
-                  width={24}
-                  height={24}
-                />
-              </div>
+    <section>
+      <div className="main-container bg-[#F7F7FB] w-full max-w-[1440px] h-auto p-[100px]">
+        {roomDataArray.map((roomData, index) => (
+          <div
+            key={index}
+            className="search-room-card bg-[#F7F7FB] h-[400px] flex justify-between font-sans py-10 border-b border-[#E4E6ED]"
+          >
+            <div className="image-box w-full max-w-[453px] h-auto relative mr-8">
+              <Image
+                src={roomData.imageSrc}
+                alt={roomData.imageAlt}
+                width={453}
+                height={320}
+                className="object-cover h-full"
+              />
+              <Link key={index} href={`/fullview/${index}`}>
+                <div className="transparent-bg w-10 h-10 bg-white flex items-center justify-center opacity-50 absolute bottom-0">
+                  <div className="frame-link-image">
+                    <Image
+                      src="/Frame.png"
+                      alt="See more image"
+                      width={24}
+                      height={24}
+                    />
+                  </div>
+                </div>
+              </Link>
             </div>
-            </Link>
+            {/* RoomDetails children component */}
+            <RoomDetails
+              roomTypeTitle={roomData.roomTypeTitle}
+              guests={roomData.guests}
+              bedType={roomData.bedType}
+              roomArea={roomData.roomArea}
+              description={roomData.description}
+            />
+            {/* PriceDetails children component */}
+            <PriceDetails
+              fullPrice={roomData.fullPrice}
+              discountPrice={roomData.discountPrice}
+              status={roomData.status}
+            />
           </div>
-          {/* RoomDetails children component */}
-          <RoomDetails
-            roomTypeTitle={roomData.roomTypeTitle}
-            guests={roomData.guests}
-            bedType={roomData.bedType}
-            roomArea={roomData.roomArea}
-            description={roomData.description}
-          />
-          {/* PriceDetails children component */}
-          <PriceDetails
-            fullPrice={roomData.fullPrice}
-            discountPrice={roomData.discountPrice}
-            status={roomData.status}
-          />
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </section>
   );
 }
