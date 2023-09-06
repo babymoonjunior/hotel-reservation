120a7422463c59cabc898ed6129f316ef13f9a58
Michael <sitthisaklo@kkumail.com>
2023-09-06 12:54:38 +0700

    feat:adding-Pop-Up-detail-for-the-first-time

--- client/src/app/(Users)/(Home)/search/page.js
+++ client/src/app/(Users)/(Home)/search/page.js
@@ -1,20 +1,21 @@
 //localhost:3000/search
 
-import '@/app/globals.css'
+import "@/app/globals.css";
 
 export const metadata = {
-  title: 'Search Room',
-  description: 'ค้นหาห้องพัก',
-}
+  title: "Search Room",
+  description: "ค้นหาห้องพัก",
+};
 
 export default function Searchpage() {
   return (
     <>
-      <h2 className=" font-sans bg-orange-300 h-[500px]">Search Results ผลการค้นหา</h2>
+      <h2 className=" font-sans bg-orange-300 h-[500px]">
+        Search Results ผลการค้นหา
+      </h2>
       {/* ใส่ Component2 <Searchbar /> ที่นี่ (Nu) */}
       {/* <Searchbar /> */}
       {/* ใส่ Component3 <Searchresults /> ที่นี่ (Wen) */}
-
     </>
   );
 }
