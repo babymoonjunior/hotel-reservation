931e1c4b8b48c56d536cb3708d678ae6f85d4c53
PreeyanuchYu <babymoonshop@gmail.com>
2023-09-07 01:23:02 +0700

    Feat: Add Link for trial

--- client/src/app/(Users)/components/Modal.jsx
+++ client/src/app/(Users)/components/Modal.jsx
@@ -8,6 +8,7 @@ export default function Modal({ children }) {
         href="/search"
         className="modal w-full h-screen bg-black fixed top-0 left-0 z-[99]"
         // style={{ background: "rgba(0, 0, 0, 0.3)" }}
+        scroll={false}
         
       />
       {children}
