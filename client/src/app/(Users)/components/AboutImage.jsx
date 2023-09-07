ba33793aa11441e232719da6bb8e03e31720c8b9
Michael <sitthisaklo@kkumail.com>
2023-09-07 23:21:42 +0700

    editing-About

--- client/src/app/(Users)/components/AboutImage.jsx
+++ client/src/app/(Users)/components/AboutImage.jsx
@@ -1,30 +1,9 @@
 import React from "react";
-import getRoomDetail from "@/lib/getRoomDetail";
 
-const AboutImage = ({ customers }) => {
-  const mockImages = [
-    "https://placedog.net/640/480?random",
-    "https://placedog.net/640/480?random",
-    "https://placedog.net/640/480?random",
-    "https://placedog.net/640/480?random",
-    "https://placedog.net/640/480?random",
-    "https://placedog.net/640/480?random",
-    "https://placedog.net/640/480?random",
-    "https://placedog.net/640/480?random",
-  ];
-
+const AboutImage = ({ item }) => {
   return (
     <div className="w-[400px] inline-block cursor-pointer relative p-2">
-      <img
-        className="w-[400px]  h-[500px] block "
-        src={roomImage.room_image}
-        alt={roomImage.first_name}
-      />
-      <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-        <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-          {roomImage.first_name}
-        </p>
-      </div>
+      <img className="w-[400px]  h-[500px] block " src={item} alt={""} />
     </div>
   );
 };
