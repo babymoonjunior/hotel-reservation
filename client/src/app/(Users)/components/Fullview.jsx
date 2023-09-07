3531113baedd0acc395ad75386527c9a5f5772bf
PreeyanuchYu <babymoonshop@gmail.com>
2023-09-07 11:52:26 +0700

    Style: Fullview.jsx 2nd Draft

--- client/src/app/(Users)/components/Fullview.jsx
+++ client/src/app/(Users)/components/Fullview.jsx
@@ -1,79 +1,105 @@
 "use client";
 
+import Image from "next/image";
 import React, { useState } from "react";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
-import Image from "next/image";
 
-export default function Fullviewimage() {
-  const slides = [
-    {
-      url: "/superior-garden-view-full.png",
-    },
-    {
-      url: "/deluxe-full.png",
-    },
-    {
-      url: "/superior-full.png",
-    },
+export default function ImageSlide() {
 
-    {
-      url: "/supreme-full.png",
-    },
-    {
-      url: "/SuperiorGardenView-1024x683.jpg",
-    },
-  ];
-
+  const roomImage = [
+        {
+          url: "/superior-garden-view-full.png",
+        },
+        {
+          url: "/deluxe-full.png",
+        },
+        {
+          url: "/superior-full.png",
+        },
+    
+        {
+          url: "/supreme-full.png",
+        },
+        {
+          url: "/SuperiorGardenView-1024x683.jpg",
+        },
+      ];
+  
   const [currentIndex, setCurrentIndex] = useState(0);
 
+// function กดเลื่อนรูปซ้ายขวา
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    const newIndex = isFirstSlide ? roomImage.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
 
   const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
+    const isLastSlide = currentIndex === roomImage.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex);
   };
 
-  const goToSlide = (slideIndex) => {
+// function กดไข่ปลาดูรูปที่ต้องการ
+    const goToSlide = (slideIndex) => {
     setCurrentIndex(slideIndex);
   };
 
   return (
-    <div className="bg-green-500 max-w-[1400px] max-h-[900px] w-full h-auto m-auto py-16 px-4 group flex flex-col items-center ">
-      <div className=" bg-red-300 w-[50%] h-[50%] rounded-2xl bg-center bg-cover duration-500 relative">
+    <div className="relative flex w-full mx-auto overflow-hidden max-w-7xl bg-black">
+      <div className="relative flex w-full max-w-xl gap-10 mx-auto">
+        <div className="absolute z-20 w-full transition-opacity duration-500 -translate-x-1/2 bg-black opacity-50 h-96 -left-80">
+        </div>
+
         <Image
-          src={slides[currentIndex].url}
-          alt="superior-garden-view-full" //ควรเอาไปใส่ในอะเรย์
-          width={930}
-          height={581}
-          className="w-full h-full object-cover "
+          src={roomImage[currentIndex - 1 < 0 ? roomImage.length - 1 : currentIndex - 1].url}
+                      
+          alt={`image`}
+          className="absolute object-cover w-full transition-opacity duration-500 -translate-x-1/2 -left-80 h-96"
+          width={300}
+          height={300}
         />
-      
-      {/* Left Arrow : Previous Image (-1) */}
-      <div className="hidden group-hover:block absolute top-[45%] text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <Image
+          src={roomImage[currentIndex].url}
+          alt={`image`}
+          className="object-cover w-full transition-opacity duration-500 h-96"
+          width={300}
+          height={300}
+        />
+        {/* เพิ่มไข่ปลาเลื่อนรูป */}
+        <div className="flex justify-center items-center py-2 absolute bottom-0 left-[40%]">
+         {roomImage.map((slide, slideIndex) => (
+          <div
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+              className="opacity-30 hover:opacity-100 text-2xl cursor-pointer"
+            >
+              <RxDotFilled />
+            </div>
+          ))}
+        </div>
+
+        <div className="absolute z-20 w-full transition-opacity duration-500 translate-x-1/2 bg-black opacity-50 h-96 left-80"></div>
+        <Image
+          src={roomImage[(currentIndex + 1) % roomImage.length].url}
+          alt={`image`}
+          className="absolute object-cover w-full transition-opacity duration-500 translate-x-1/2 left-80 h-96"
+          width={300}
+          height={300}
+        />
+      </div>
+
+      {/* ใส่ปุ่มเลื่อนรูปซ้ายขวา */}
+      <div
+        className="absolute z-30 -translate-y-1/2 cursor-pointer top-1/2 left-64 text-2xl rounded-full p-2 bg-black/20 text-white"
+      >
         <BsChevronCompactLeft onClick={prevSlide} size={30} />
       </div>
-      {/* Right Arrow : Next Image (+1) */}
-      <div className="hidden group-hover:block absolute top-[45%] right-0 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+      <div
+        className="absolute z-30 -translate-y-1/2 cursor-pointer top-1/2 right-64 text-2xl rounded-full p-2 bg-black/20 text-white"
+      >
         <BsChevronCompactRight onClick={nextSlide} size={30} />
-      </div>
-      
-      <div className="flex justify-center items-center py-2 absolute bottom-0 left-[45%]">
-        {slides.map((slide, slideIndex) => (
-          <div
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-            className="opacity-30 hover:opacity-100 text-2xl cursor-pointer"
-          >
-            <RxDotFilled />
-          </div>
-        ))}
-      </div>
       </div>
     </div>
   );
