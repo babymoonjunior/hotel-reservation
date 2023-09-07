aae1768ac91ef2fe25b30af53560ef88c4cef7c1
Nu <nuuseforstudy@gmail.com>
2023-09-07 20:50:08 +0700

    feat: searchpage

--- client/src/app/(Users)/(Home)/search/page.js
+++ client/src/app/(Users)/(Home)/search/page.js
@@ -1,20 +1,46 @@
 //localhost:3000/search
-
-import '@/app/globals.css'
+import "@/app/globals.css";
+import SearchBar from "@/components/SearchBar";
 
 export const metadata = {
-  title: 'Search Room',
-  description: 'ค้นหาห้องพัก',
-}
+  title: "Search Room",
+  description: "ค้นหาห้องพัก",
+};
 
 export default function Searchpage() {
   return (
     <>
-      <h2 className=" font-sans bg-orange-300 h-[500px]">Search Results ผลการค้นหา</h2>
       {/* ใส่ Component2 <Searchbar /> ที่นี่ (Nu) */}
+      <div>
+        {" "}
+        <SearchBar page="searchpage" />
+      </div>
+
+      <h2 className="font-sans bg-orange-300 ">Search Results ผลการค้นหา</h2>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
+      <h1 className="text-5xl">asdfasdfasdf</h1>
       {/* <Searchbar /> */}
       {/* ใส่ Component3 <Searchresults /> ที่นี่ (Wen) */}
-
     </>
   );
 }
