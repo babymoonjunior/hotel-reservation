52bfcddf71dcbee7f2bb1990892ea127c4149891
PreeyanuchYu <babymoonshop@gmail.com>
2023-09-15 11:50:17 +0700

    Feat: Edit ChangeDate Page & Add Popup & Edit Calendar

--- client/src/app/(Users)/components/ChangeDate.jsx
+++ client/src/app/(Users)/components/ChangeDate.jsx
@@ -1,10 +1,49 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { ChangeDatePicker } from "./ChangeDatePicker";
+import React, { useState } from "react";
+import ChangeDatePopUp from "./ChangeDatePopUp";
+import { useRouter } from "next/navigation";
+import { format } from "date-fns";
 
 export default function ChangeDate() {
+  const [showCancelModal, setShowCancelModal] = useState(false);
+  const [showChangeDateModal, setShowChangeDateModal] = useState(false);
+  const [isPopUpVisible, setIsPopUpVisible] = useState(false);
+  const router = useRouter();
+  const [selectedDate, setSelectedDate] = useState(null); // เพิ่ม state สำหรับเก็บค่า date
+  const [checkInDate, setCheckInDate] = useState(null);
+  const [checkOutDate, setCheckOutDate] = useState(null);
+
+  let originalCheckIn = "Sun, 22 Oct 2023";
+  let originalCheckOut = "Thu, 26 Oct 2023";
+  let isCheckIn = false;
+  let isCheckOut = false;
+
+  // สร้างฟังก์ชันเพื่อรับค่า date จาก ChangeDatePicker component
+  const handleCheckInDateChange = (date) => {
+    setCheckInDate(date);
+  };
+  const handleCheckOutDateChange = (date) => {
+    setCheckOutDate(date);
+  };
+
+  // สร้างฟังก์ชันเพื่อแสดง PopUp
+  const showChangeDatePopUp = () => {
+    setShowChangeDateModal(true);
+    setIsPopUpVisible(true);
+  };
+
+  const showCancelPopUp = () => {
+    setShowCancelModal(true);
+    setIsPopUpVisible(false); //ตั้งค่าไม่โชว์ pop up ไว้
+    router.push("/bookinghistory");
+  };
+
   return (
-    <section className="flex flex-col justify-start items-center px-[72px] max-w-[1440px] h-[1028px] bg-[#F1F2F6]">
+    <section className="flex flex-col justify-start items-center px-[72px] max-w-[1440px] h-auto bg-[#F1F2F6]">
       <h1 className="w-[57%] font-mono text-[68px] font-medium leading-[125%] tracking-[-1.36px] self-start py-12">
         Change Check-in and Check-out Date
       </h1>
@@ -38,7 +77,7 @@ export default function ChangeDate() {
                   Original Date
                 </p>
                 <span className="text-[#424C6B] font-sans text-base font-normal leading-[150%] tracking-[-0.32px]">
-                  Th, 19 Oct 2022 - Th, 19 Oct 2022
+                  {originalCheckIn} - {originalCheckOut}
                 </span>
               </div>
             </div>
@@ -50,24 +89,49 @@ export default function ChangeDate() {
               </p>
               <div className="flex flex-row justify-between mt-4">
                 <div>
-                  <span className="text-[#2A2E3F] font-sans text-base font-normal leading-[150%] py-2">
-                    Check In
+                  <span className="text-[#2A2E3F] font-sans text-base font-normal leading-[150%] py-3">
+                    Check In :
+                    <span className="text-white">
+                      {" "}
+                      {checkInDate
+                        ? format(checkInDate, "EEE, dd MMM yyyy")
+                        : "Pick a date"}
+                    </span>
                   </span>
-                  <ChangeDatePicker />
+                  {/* เรียกใช้ ChangeDatePicker และส่งค่า checkInDate และ handleCheckInDateChange ไป */}
+                  <ChangeDatePicker
+                    isCheckIn={true}
+                    date={checkInDate}
+                    onDateChange={handleCheckInDateChange}
+                    originalCheckIn={originalCheckIn} // เพิ่ม props สำหรับ originalCheckIn
+                    originalCheckOut={originalCheckOut} // เพิ่ม props สำหรับ originalCheckOut
+                  />
                 </div>
                 <div className="text-[#2A2E3F] flex justify-center items-center ">
                   -
                 </div>
                 <div>
-                  <span className="text-[#2A2E3F] font-sans text-base font-normal leading-[150%] py-2">
-                    Check Out
+                  <span className="text-[#2A2E3F] font-sans text-base font-normal leading-[150%] py-3">
+                    Check Out :{" "}
+                    <span className="text-white">
+                      {" "}
+                      {checkOutDate
+                        ? format(checkOutDate, "EEE, dd MMM yyyy")
+                        : "Pick a date"}
+                    </span>
                   </span>
-                  <ChangeDatePicker />
+                  {/* เรียกใช้ ChangeDatePicker และส่งค่า checkOutDate และ handleCheckOutDateChange ไป */}
+                  <ChangeDatePicker
+                    isCheckOut={true}
+                    date={checkOutDate}
+                    onDateChange={handleCheckOutDateChange}
+                    originalCheckIn={originalCheckIn} // เพิ่ม props สำหรับ originalCheckIn
+                    originalCheckOut={originalCheckOut} // เพิ่ม props สำหรับ originalCheckOut
+                  />
                 </div>
               </div>
             </div>
             {/* End Change Date Calendar */}
-
           </div>
           {/* End booking-details-section */}
         </div>
@@ -75,24 +139,61 @@ export default function ChangeDate() {
 
         {/* button group */}
         <div className="button-group flex flex-row justify-between pt-5 pb-10">
-          <div className="left-btn">
-            <Button
-              variant="ghost"
-              className="Cancel-Booking-Btn bg-[#F1F2F6] text-base not-italic font-semibold leading-4 w-fit"
-            >
-              Cancel
-            </Button>
-          </div>
-          <div className="right-btn-group">
-            <Button className="Change-Date-Btn text-base not-italic font-semibold leading-4 w-fit">
-              Confirm Change Date
-            </Button>
-          </div>
+          {isPopUpVisible ? (
+            <div className="left-btn">
+              <Button
+                variant="ghost"
+                className="Cancel-Booking-Btn bg-[#F1F2F6] text-base not-italic font-semibold leading-4 w-fit"
+                disabled
+              >
+                Cancel
+              </Button>
+            </div>
+          ) : (
+            <div className="left-btn">
+              <Button
+                variant="ghost"
+                className="Cancel-Booking-Btn bg-[#F1F2F6] text-base not-italic font-semibold leading-4 w-fit"
+                onClick={showCancelPopUp}
+              >
+                Cancel
+              </Button>
+            </div>
+          )}
+
+          {isPopUpVisible ? (
+            <div className="right-btn-group">
+              <Button
+                className="Change-Date-Btn text-base not-italic font-semibold leading-4 w-fit"
+                disabled
+              >
+                Confirm Change Date
+              </Button>
+            </div>
+          ) : (
+            <div className="right-btn-group">
+              <Button
+                className="Change-Date-Btn text-base not-italic font-semibold leading-4 w-fit"
+                onClick={showChangeDatePopUp}
+              >
+                Confirm Change Date
+              </Button>
+            </div>
+          )}
         </div>
         {/* End button group */}
-
       </div>
       {/* End history-card */}
+      {isPopUpVisible && (
+        <ChangeDatePopUp
+          showCancelModal={showCancelModal}
+          setShowCancelModal={setShowCancelModal}
+          showChangeDateModal={showChangeDateModal}
+          setShowChangeDateModal={setShowChangeDateModal}
+          isPopUpVisible={isPopUpVisible}
+          setIsPopUpVisible={setIsPopUpVisible}
+        />
+      )}
     </section>
   );
 }
--- client/src/app/(Users)/components/ChangeDatePopUp.jsx
+++ client/src/app/(Users)/components/ChangeDatePopUp.jsx
@@ -1,8 +1,20 @@
+"use client";
+
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 
-export default function ChangeDatePopUp() {
-  let changeDate = false; // สมมติว่ามีการกดปุ่ม change date
-  let cancel = true; // สมมติว่ามีการกดปุ่ม cancel
+export default function ChangeDatePopUp(props) {
+  const {
+    showCancelModal,
+    setShowCancelModal,
+    showChangeDateModal,
+    setShowChangeDateModal,
+    isPopUpVisible,
+    setIsPopUpVisible,
+  } = props;
+
+  // let changeDate = false; // สมมติว่ามีการกดปุ่ม change date
+  // let cancel = true; // สมมติว่ามีการกดปุ่ม cancel
   let refund = false; // สมมติว่าขอคืนเงินได้
 
   let title = "";
@@ -10,44 +22,74 @@ export default function ChangeDatePopUp() {
   let leftButtonText = "";
   let rightButtonText = "";
 
-  if (changeDate) {
+  if (showChangeDateModal) {
     title = "Change Date";
     questionText =
       "Are you sure you want to change your check-in and check-out date?";
     leftButtonText = "No, I don’t";
     rightButtonText = "Yes, I want to change";
-  } else if (cancel) {
+  } else if (showCancelModal) {
     if (refund) {
       title = "Cancel Booking";
       questionText =
         "Are you sure you want to cancel this booking and request a refund?";
-      leftButtonText = "Yes, I want to cancel and request refund";
-      rightButtonText = "No, Don’t Cancel";
+      leftButtonText = "No, Don’t Cancel";
+      rightButtonText = "Yes, I want to cancel and request refund";
     } else {
       title = "Cancel Booking";
       questionText =
         "Cancellation of the booking now will not be able to request a refund. Are you sure you would like to cancel this booking?";
-      leftButtonText = "Yes, I want to cancel without refund";
-      rightButtonText = "No, Don’t Cancel";
+      leftButtonText = "No, Don’t Cancel";
+      rightButtonText = "Yes, I want to cancel without refund";
     }
   }
 
   return (
     <>
-      <div className="popup bg-white max-w-[550px] w-full font-sans drop-shadow-xl">
+      <div
+        className="popup bg-white max-w-[550px] w-full font-sans drop-shadow-xl z-50 absolute left-[50%] -bottom-[30%]"
+        style={{
+          transform: "translate(-50%, -50%)",
+        }}
+      >
         <div className="flex flex-row justify-between items-center border-b border-[#E4E6ED] px-4 py-2">
           <span className="modal-titile text-xl font-semibold leading-[150%] tracking-[-0.4px]">
             {title}
           </span>
-          <span className="text-[#C8CCDB]">X</span>
+          <button
+            className="text-[#C8CCDB]"
+            onClick={() => {
+              if (showChangeDateModal) {
+                setIsPopUpVisible(false); // ปิด PopUp สำหรับ Change Date
+                setShowChangeDateModal(false);
+              } else if (showCancelModal) {
+                setIsPopUpVisible(false); // ปิด PopUp สำหรับ Cancel
+                setShowCancelModal(false);
+              }
+            }}
+          >
+            X
+          </button>
         </div>
         <div className="p-4">
           <p className="modal-question text-[#646D89] text-base font-normal leading-[150%] tracking-[-0.32px] pb-4">
             {questionText}
           </p>
           <div className="flex flex-row justify-end">
-            <Button variant="secondary" className="left-button w-fit mr-3">
-            {leftButtonText}
+            <Button
+              variant="secondary"
+              className="left-button w-fit mr-3"
+              onClick={() => {
+                if (showChangeDateModal) {
+                  setIsPopUpVisible(false); // ปิด PopUp สำหรับ Change Date
+                  setShowChangeDateModal(false);
+                } else if (showCancelModal) {
+                  setIsPopUpVisible(false); // ปิด PopUp สำหรับ Cancel
+                  setShowCancelModal(false);
+                }
+              }}
+            >
+              {leftButtonText}
             </Button>
             <Button className="right-button w-fit">{rightButtonText}</Button>
           </div>
