fb0b4421124598273a0e09b6e80977fab5914e22
BuildTools <unconfigured@null.spigotmc.org>
2023-09-18 10:44:20 +0700

    feat(Sidebar):add sidebar menu color

--- client/src/app/(Users)/components/(dashboard)/Sidebar.jsx
+++ client/src/app/(Users)/components/(dashboard)/Sidebar.jsx
@@ -1,9 +1,18 @@
 "use client";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function Sidebar() {
+  const pathname = usePathname();
+  const router = useRouter();
+
   const buttonMenuStyle =
     "w-full h-16 hover:bg-green-600 active:bg-green-700 font-[500px] text-sm  pl-4";
+
+  const isPathnameActive = (path) => {
+    return pathname === path;
+  };
+
   return (
     <>
       <div className=" flex flex-col w-[240px] h-[1024px]  bg-green-800 text-gray-300 fixed">
@@ -19,25 +28,45 @@ export default function Sidebar() {
           </div>
         </div>
         <div className="basis-1/2 flex flex-col justify-start">
-          <button className={buttonMenuStyle}>
+          <button
+            className={`${buttonMenuStyle} ${
+              isPathnameActive("/dashboard/booking") &&
+              "bg-green-600 hover:bg-green-600 active:bg-green-600"
+            }`}
+          >
             <div className="flex gap-5 items-center p-4">
               <Image src="/booking-invert.svg" width={24} height={24} />
               Cutomer Booking
             </div>
           </button>
-          <button className={buttonMenuStyle}>
+          <button
+            className={`${buttonMenuStyle} ${
+              isPathnameActive("/dashboard/manage") &&
+              "bg-green-600 hover:bg-green-600 active:bg-green-600"
+            }`}
+          >
             <div className="flex gap-5 items-center p-4">
               <Image src="/manage-invert.svg" width={24} height={24} />
               Room Management
             </div>
           </button>
-          <button className={buttonMenuStyle}>
+          <button
+            className={`${buttonMenuStyle} ${
+              isPathnameActive("/dashboard/hotel") &&
+              "bg-green-600 hover:bg-green-600 active:bg-green-600"
+            }`}
+          >
             <div className="flex gap-5 items-center p-4">
               <Image src="/hotel-invert.svg" width={24} height={24} />
               Hotel Infomation
             </div>
           </button>
-          <button className={buttonMenuStyle}>
+          <button
+            className={`${buttonMenuStyle} ${
+              isPathnameActive("/dashboard/room") &&
+              "bg-green-600 hover:bg-green-600 active:bg-green-600"
+            }`}
+          >
             <div className="flex gap-5 items-center p-4">
               <Image src="/room-invert.svg" width={24} height={24} />
               Room & Property
