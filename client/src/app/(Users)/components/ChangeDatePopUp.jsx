725ccffa387646944d95d259be115df7d0ea9dc1
Nu <nuuseforstudy@gmail.com>
2023-09-20 10:47:03 +0700

    feat: delete  useeffect

--- client/src/app/(Users)/components/ChangeDatePopUp.jsx
+++ client/src/app/(Users)/components/ChangeDatePopUp.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
