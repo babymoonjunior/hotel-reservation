ce070757b6a6d51918573116003de364ba35dd99
Puttipong <maxnetoogo@gmail.com>
2023-09-21 11:26:31 +0700

    feat: commit

--- client/src/app/(Users)/(Register)/paymentupdate/page.jsx
+++ client/src/app/(Users)/(Register)/paymentupdate/page.jsx
@@ -1,30 +1,94 @@
 "use client"
-import React from 'react';
-import { useState, useEffect } from "react";
-import { useForm, Controller } from "react-hook-form";
-import Image from "next/image";
-import imgRegister from "../../../../../public/imgRegister.svg";
-import countryData from "../../components/countryData.json";
-import Link from "next/link";
+
+import { useState, useEffect} from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useRouter } from "next/navigation";
-// import { supabase } from '@/supabase';
 
 export default function paymentUpdatePage() {
+    const supabase = createClientComponentClient()
+    const [loading, setLoading] = useState(true)
+    const [cardNumber, setCardNumber] = useState("")
+    const [cardOwner, setCardOwner] = useState("")
+    const [cardExpire, setCardExpire] = useState("")
+    const [cardCVC, setCardCVC] = useState("")
+    const [user, setUser] = useState(null)
+    const [formErrors, setFormErrors] = useState(null);
 
-//   const fetchOrders = async () => {
-//     try {
-//         let data = await supabase
-//             .from('profile')
-//             .select('*')
-//     }
-//     catch(error) {
+    useEffect(() => {
+        const checkLoginStatus = async () => {
+          try {
+            setLoading(true);
+            const { data, error } = await supabase.auth.getSession();
+            console.log("data:", data);
+            if (error) {
+              alert(error.message);
+            }
+            if (data.session === null) {
+              window.location.href = "/login";
+            } else {
+              setUser(data.session.user);
+              setLoading(false);
+            }
+          } catch (error) {
+            console.error("Error fetching login status:", error);
+          }
+        };
+        checkLoginStatus();
+      }, []);
 
-//     }
-//   }
+    useEffect(() => {
+        const fetchSmoothie = async () => {
+          const { data, error } = await supabase
+            .from("profiles")
+            .select('card_number, card_owner, card_expire, card_cvc')
+            .eq("id", user?.id)
+            .single();
+          if (error) {
+            throw error;
+          }
+          if (data) {
+            setCardNumber(data.card_number);
+            setCardOwner(data.card_owner);
+            setCardExpire(data.card_expire);
+            setCardCVC(data.card_cvc);        
+          }
+        };
+    
+        fetchSmoothie();
+    }, [user?.id]);
+    
+    const updateData = async (e) => {
+        e.preventDefault()     
+        
+        const { data, error } = await supabase
+          .from("profiles")
+          .update({ 
+            card_number: cardNumber, 
+            card_owner: cardOwner, 
+            card_expire: cardExpire, 
+            card_cvc: cardCVC 
+          })
+          .eq("id", user?.id)
+          .select();
 
-  return (
-    <div className="h-[1087px] w-screen relative bg-slate-50">
+        if (error || data === "undefiend") {
+          console.log(error);
+          setFormErrors({
+            error: "Please enter a 16-digit card number",
+            cardO: "Please enter name card owner",
+            cardE: "Please enter card expire MM/YY",
+            cardC: "Please enter a 3-digit CVC/CVV",
+        });
+        } 
+        else {
+          alert(`PaymentUpdated Successful!`);
+        }   
+    }
+
+    return (
+    <form 
+        onSubmit={updateData}
+        className="h-[1087px] w-screen relative bg-slate-50">
+    
         <div className="max-w-[930px] mx-auto pl-[1px] pr-[2px]
              bg-slate-50 my-12 mt-20">
 
@@ -35,10 +99,12 @@ export default function paymentUpdatePage() {
                         Payment Method
                 </h1>
                 <button 
-                    className='bg-orange-600 px-[34px] h-[48px] rounded-[4px]'>
+                    type="submit"
+                    className='bg-orange-600 px-[34px] h-[48px] rounded-[4px]'
+                >               
                     <h1 className='text-white text-[16px] font-openSans font-semibold
                      leading-[16px] break-words'>
-                        Update payment method
+                        {loading ? 'Loading ...' : 'Update payment method'}
                     </h1> 
                 </button>           
             </div>
@@ -48,10 +114,10 @@ export default function paymentUpdatePage() {
                     Credit Card
                 </h4>
             </div>
-            <form className="grid md:grid-cols-[52%_52%]">               
+            <div className="grid md:grid-cols-[52%_52%]">               
                 <div className='flex flex-col w-full gap-2 mt-10 md:w-11/12'>
                     <label 
-                        htmlFor="card-number"
+                        htmlFor="cardNumber"
                         className="text-gray-900 text-[16px] font-sans leading-[24px]
                          font-normal break-words" >
                             Card Number                      
@@ -61,11 +127,20 @@ export default function paymentUpdatePage() {
                         placeholder-slate-400 rounded focus:outline-none
                         focus:border-orange-400 focus:ring-1 focus:ring-orange-400
                         disabled:shadow-none border-gray-400 break-words`} 
+                        id="cardNumber"
+                        type="text"
+                        value={cardNumber || ''}
+                        onChange={(e) => setCardNumber(e.target.value)}
+                        placeholder="Enter card number"
+                        pattern= "[0-9]{16}"
+                        required
                     />
-                </div>   
+                    <p className="text-red-500">{formErrors?.cardN}</p>
+                </div> 
+
                 <div className='flex flex-col w-full gap-2 mt-10 md:w-11/12'>
                     <label 
-                        htmlFor="card-owner"
+                        htmlFor="cardOwner"
                         className="text-gray-900 text-[16px] font-sans leading-[24px]
                          font-normal break-words" >
                             Card Owner
@@ -74,12 +149,22 @@ export default function paymentUpdatePage() {
                         text-[16px] font-sans leading-6 p-[12px] text-black
                         placeholder-slate-400 rounded focus:outline-none
                         focus:border-orange-400 focus:ring-1 focus:ring-orange-400
-                        disabled:shadow-none border-gray-400 break-words`} 
+                        disabled:shadow-none border-gray-400 break-words
+                        `} 
+                        id="cardOwner"
+                        type="text"
+                        value={cardOwner || ''}
+                        onChange={(e) => setCardOwner(e.target.value)}
+                        placeholder="Enter card Owner"
+                        pattern= "^[a-zA-Z]+$"
+                        required
                     />
-                </div>   
+                    <p className="text-red-500">{formErrors?.cardO}</p>
+                </div> 
+
                 <div className="flex flex-col w-full gap-2 mt-10 md:w-11/12">
                     <label 
-                        htmlFor="expiry-date"
+                        htmlFor="cardExpire"
                         className="text-gray-900 text-[16px] font-sans leading-[24px]
                          font-normal break-words" >
                             Expiry Date
@@ -89,11 +174,20 @@ export default function paymentUpdatePage() {
                         placeholder-slate-400 rounded focus:outline-none
                         focus:border-orange-400 focus:ring-1 focus:ring-orange-400
                         disabled:shadow-none border-gray-400 break-words`} 
+                        id="cardExpire"
+                        type="text"
+                        value={cardExpire || ''}
+                        onChange={(e) => setCardExpire(e.target.value)}
+                        placeholder="Enter card Expire"
+                        pattern="^\d{2}\/\d{2}$"
+                        required
                     />
-                </div>   
+                    <p className="text-red-500">{formErrors?.cardE}</p>
+                </div>  
+
                 <div className="flex flex-col w-full gap-2 mt-10 md:w-11/12">
                     <label 
-                        htmlFor="fullName"
+                        htmlFor="cardCvc"
                         className="text-gray-900 text-[16px] font-sans leading-[24px]
                          font-normal break-words" >                       
                             CVC/CVV
@@ -103,10 +197,19 @@ export default function paymentUpdatePage() {
                         placeholder-slate-400 rounded focus:outline-none
                         focus:border-orange-400 focus:ring-1 focus:ring-orange-400
                         disabled:shadow-none border-gray-400 break-words`} 
+                        id="cardCvc"
+                        type="text"
+                        value={cardCVC || ''}
+                        onChange={(e) => setCardCVC(e.target.value)}
+                        placeholder="Enter card CVC/CVV"
+                        pattern="[0-9]{3}"
+                        required
                     />
-                </div>                
-            </form>
+                    <p className="text-red-500">{formErrors?.cardC}</p>
+                </div> 
+
+            </div>
         </div>
-    </div>
+    </form>
     )
 }
