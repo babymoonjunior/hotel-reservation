feb893dad0900bf6dcadc2bb49565596848f2f2e
BuildTools <unconfigured@null.spigotmc.org>
2023-09-21 14:11:01 +0700

    feat(manage): add status

--- client/src/app/(Users)/components/(dashboard)/RoomManageBody.jsx
+++ client/src/app/(Users)/components/(dashboard)/RoomManageBody.jsx
@@ -1,22 +1,48 @@
 "use client";
-import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function RoomManageBody() {
   const [room, setRoom] = useState([]);
+  const [roomStatus, setRoomStatus] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
+  const [order, setOrder] = useState("room_id");
+  const [ascending, setAscending] = useState(true);
   const supabase = createClientComponentClient();
+
+  const statusColors = {
+    Vacant: "bg-green-500",
+    Dirty: "bg-red-500",
+    Occupied: "bg-red-500",
+    "Assign Clean": "bg-green-500",
+    "Assign Dirty": "bg-red-500",
+    "Out of Service": "bg-gray-500",
+  };
 
   const getData = async () => {
     try {
       const result = await supabase
         .from("rooms")
         .select(
-          `room_id,room_number,room_status,room_types(room_type_id,roomtypetitle,bedtype)`
-        );
-
+          `room_id,room_number,room_types(room_type_id,roomtypetitle,bedtype),room_status(room_status_id,room_status)`
+        )
+        // room_id,room_number,room_status,room_types(room_type_id,roomtypetitle,bedtype),room_status_types(id,room_status)
+        .order("room_id", { ascending: true });
+      // .order("room_types(room_type_id)", { ascending: true });
+      // .order("room_types(bedtype)", { ascending: true });
+      // .order("room_status", { ascending: true });
+      const roomStatusData = await supabase
+        .from("room_status")
+        .select("room_status");
+      setRoomStatus(roomStatusData.data);
+      console.log(roomStatusData.data);
       setRoom(result.data);
       console.log(result.data);
     } catch (error) {
@@ -44,14 +70,40 @@ export default function RoomManageBody() {
               <>
                 <div
                   className="flex justify-between items-center bg-utility-white py-2 px-5 w-full h-[70px] mb-1 shadow-sm"
-                  id="room.room_id"
+                  id={room.room_id}
                 >
                   <div className="basis-1/5">{room.room_number}</div>
                   <div className="basis-3/5">
                     {room.room_types.roomtypetitle}
                   </div>
                   <div className="basis-2/5">{room.room_types.bedtype}</div>
-                  <div className="basis-2/5">{room.room_status}</div>
+                  <div className="basis-2/5">
+                    <DropdownMenu className="outline-none">
+                      <DropdownMenuTrigger
+                        className={`outline-none px-2 py-1 rounded ${
+                          statusColors[room.room_status.room_status]
+                        }`}
+                      >
+                        {room.room_status.room_status}
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent className="bg-utility-white border-none">
+                        {roomStatus.map((status) => {
+                          return (
+                            <>
+                              <DropdownMenuItem
+                                id={status.room_status}
+                                className={`px-2 py-1 rounded ${
+                                  statusColors[status.room_status]
+                                }`}
+                              >
+                                {status.room_status}
+                              </DropdownMenuItem>
+                            </>
+                          );
+                        })}
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </div>
                 </div>
               </>
             );
