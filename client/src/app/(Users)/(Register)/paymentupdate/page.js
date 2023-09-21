3c056cbd5bd4ca3f7711787bd019cb294b640756
Puttipong <maxnetoogo@gmail.com>
2023-09-21 11:26:31 +0700

    feat: updatepayment

---
+++ client/src/app/(Users)/(Register)/paymentupdate/page.js
@@ -0,0 +1,23 @@
+"use client"
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="h-[187px] w-screen relative">
+        <div>  
+            <div className="flex flex-col mt-10">
+                <h1 className="font-mono font-medium text-[68px] leading-[85px] text-green-800">
+                    Payment Method
+                </h1>
+                <h4>
+                    Credit Card
+                </h4>
+            </div>
+            <form>
+                <label></label>
+                <input></input>
+            </form>
+        </div>
+    </div>
+    )
+}
