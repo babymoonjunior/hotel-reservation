73b7dc7ea13112bbaa56067ca8cd0b21b1429ea2
BuildTools <unconfigured@null.spigotmc.org>
2023-09-26 10:46:15 +0700

    feat(nav): fix logout from admin panel navbar didnt change

--- client/src/app/(Users)/components/(dashboard)/RoomManage.jsx
+++ client/src/app/(Users)/components/(dashboard)/RoomManage.jsx
@@ -90,7 +90,7 @@ export default function RoomManageBody() {
 
   return (
     <>
-      <div className="flex justify-evenly gap-20 items-center h-20 sticky top-0 px-[60px] py-4 bg-utility-white font-sans font-semibold text-xl text-gray-900 shadow">
+      <div className="flex justify-between items-center h-20 sticky top-0 px-32 py-4 bg-utility-white font-sans font-semibold text-xl text-gray-900 shadow">
         <div>Room Management</div>
         <div className="flex justify-center items-center border border-gray-400 rounded-sm h-12 px-3 py-4 gap-4">
           <div className="flex justify-center items-center object-cover">
